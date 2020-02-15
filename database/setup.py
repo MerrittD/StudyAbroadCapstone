414b6a5ecb8ab4fa0a9f6e002873a883d89c65fe
MerrittD <31487678+MerrittD@users.noreply.github.com>
2020-02-15 14:27:57 -0600

    Error: Files not found
    Mason forced a github commit, files lost, backup deployed, should be fixed.

---
+++ database/setup.py
@@ -0,0 +1,7 @@
+from admin import Admin
+from term import Term
+from programs import Program
+from locations import City, Country
+from languages import Languages,programs_languages
+from areas import Area, programs_areas
+db.create_all()
---
+++ db.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+
+app= Flask(__name__)
+url = 'sqlite:///test2.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI']=url
+db = SQLAlchemy(app)	#creats an object for SQL to be used
+
+# More info at https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#a-minimal-application
+
+
+# this was for testing and can be ignored/ used as a template for other files 
+#class User(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    username = db.Column(db.String(80), unique=True, nullable=False)
+#    email = db.Column(db.String(120), unique=True, nullable=False)
+
+#    def __repr__(self):
+#        return '<User %r>' % self.username
