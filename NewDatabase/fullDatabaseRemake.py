d3cfe670fd85bea8f6c68dd08274c05de00d570c
MerrittD <31487678+MerrittD@users.noreply.github.com>
2020-02-21 10:24:40 -0600

    More Work

--- NewDatabase/fullDatabaseRemake.py
+++ NewDatabase/fullDatabaseRemake.py
@@ -74,27 +74,28 @@ class Term(db.Model):
 		db.session.commit()
 #Old way to construct the association tables 
 	#Association table to programs.py
-	#programs_cities = db.Table('Programs_Cities', Base.metadata,
-	#	db.Column('program_id', db.Integer, db.ForeignKey('programs.id')),
-	#	db.Column('city_id', db.Integer, db.ForeignKey('city.id')))
+
+pograms_cities = db.Table('Programs_Cities',
+db.Column('program_id', db.Integer, db.ForeignKey('programs.id'),primary_key=True),
+db.Column('city_id', db.Integer, db.ForeignKey('city.id')),primary_key=True)
 
 
-	#cities_countries = db.Table('Cities_Countries', Base.metadata,
-	#	db.Column('city_id', db.Integer, db.ForeignKey('city.id')),
-	#	db.Column('country_id', db.Integer, db.ForeignKey('country.id')))
+cities_countries = db.Table('Cities_Countries',
+db.Column('city_id', db.Integer, db.ForeignKey('city.id'),primary_key=True),
+db.Column('country_id', db.Integer, db.ForeignKey('country.id'),primary_key=True))
 
 
 
-class Programs_Cities(db.Model):
+#class Programs_Cities(db.Model):
 	#Individual Attributes
-	__tablename__ = 'programs_cities'
+	#__tablename__ = 'programs_cities'
 	
-	program_id = db.Column(db.Integer, db.ForeignKey('programs.id'))
-	city_id = Column(db.Integer, db.ForeignKey('cities.id'))
+	#program_id = db.Column(db.Integer, db.ForeignKey('programs.id'))
+	#city_id = Column(db.Integer, db.ForeignKey('cities.id'))
 
 	#Relationships
-	program = db.relationship("Program",  back_populates="cities")
-	city = db.relationship("City",  back_populates="programs")
+	#program = db.relationship("Program",  back_populates="cities")
+	#city = db.relationship("City",  back_populates="programs")
 
 
 
@@ -123,17 +124,17 @@ class City(db.Model):
 		db.session.commit()
 
 
-
-class Cities_Countries(db.Model):
+#class Cities_Countries(db.Model):
 	#Individual Attributes
-	__tablename__ = 'cities_countries'
+#	__tablename__ = 'cities_countries'
 	
-	city_id = Column(db.Integer, db.ForeignKey('cities.id'))
-	country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
+#	city_id = Column(db.Integer, db.ForeignKey('cities.id'))
+#	country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
 
 	#Relationships
-	city = db.relationship("City",  back_populates="countries")
-	country = db.relationship("Country",  back_populates="cities")
+#	city = db.relationship("City",  back_populates="countries")
+
+#	country = db.relationship("Country",  back_populates="cities")
 
 
 
@@ -164,21 +165,22 @@ class Country(db.Model):
 		db.session.add(self)
 		db.session.commit()
 ##Association Table refrences back to programs
-#	programs_languages = db.Table('Programs_Languages', Base.metadata,
-#	db.Column('program_id', db.Integer, db.ForeignKey('programs.id')),
-#	db.Column('language_id', db.Integer, db.ForeignKey('languages.id')))
+
+programs_languages = db.Table('Programs_Languages',
+db.Column('program_id', db.Integer, db.ForeignKey('programs.id'),primary_key=True),
+db.Column('language_id', db.Integer, db.ForeignKey('languages.id'),primary_key=True))
 
 
-class Programs_Languages(db.Model):
+#class Programs_Languages(db.Model):
 	#Individual Attributes
-	__tablename__ = 'programs_languages'
+#	__tablename__ = 'programs_languages'
 	
-	program_id = db.Column(db.Integer, db.ForeignKey('programs.id'))
-	language_id = Column(db.Integer, db.ForeignKey('languages.id'))
+#	program_id = db.Column(db.Integer, db.ForeignKey('programs.id'))
+#	language_id = Column(db.Integer, db.ForeignKey('languages.id'))
 	
 	#Relationships
-	program = db.relationship("Program",  back_populates="languages")
-	language = db.relationship("Language",  back_populates="programs")
+#	program = db.relationship("Program",  back_populates="languages")
+#	language = db.relationship("Language",  back_populates="programs")
 
 
 
@@ -237,7 +239,7 @@ class Program(db.Model):
 	cities = db.relationship("Programs_Cities", back_populates="program")
 
 	programs_terms = db.relationship('Term', secondary=programs_terms, lazy='subquery', 
-								  backref=db.backref('program',lazy=True))
+								  backref=db.backref('Term',lazy=True))
 	
 
 
