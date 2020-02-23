64c0a0ec94fa1708e8d0725d06a81006545ed039
MerrittD <31487678+MerrittD@users.noreply.github.com>
2020-02-23 11:22:54 -0600

    Initial Psudocode
    Started looking into adding things to databases, and began thinking about how to backup/load from files. Might be best to just have the database itself fully backup and check to see if a database is present in the setup file.

--- NewDatabase/setupFile.py
+++ NewDatabase/setupFile.py
@@ -1,4 +1,23 @@
 from fullDatabaseRemake import Admin,programs_areas,programs_terms,programs_cities,programs_languages,Program,Area,Term, City,Country, Language 
 from databaseTesting import db
 
+
+
+#This file is to be run upon initial setup of the database, and should pull from a 
+#backup file inorder to populate new database 
+
 db.create_all()
+
+
+
+#Here will be a loop to add data to the database from files 
+# For every program in the file for database population
+    # create the program
+    #newprogram = Program('all things needed')
+    #db.session.add(newprogram)
+#once we are done adding programs,     
+#db.session.commit()
+#admins can be loaded in with a different loop before the commit
+#if we use a text document to populate, we can take the strings in and use .split()
+#inorder to break the strings into the needed parts
+#however, this isnt secure and there has to be a better way 
