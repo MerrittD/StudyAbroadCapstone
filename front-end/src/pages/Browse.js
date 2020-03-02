3155dce6f68b955c54b3b5be244efbff82cf67d0
MasonTDaniel <danielm@southwestern.edu>
2020-03-02 00:30:21 -0600

    changed browse from func to class
    in preparation for state and prop implementation

--- front-end/src/pages/Browse.js
+++ front-end/src/pages/Browse.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import NavBar from '../components/NavBar'
 import SearchBar from '../components/SearchBar'
 
-function Browse() {
-    return (
-        <div>
-            <NavBar />
-            <SearchBar />
-        </div>
-    )
+class Browse {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
 export default Browse;
