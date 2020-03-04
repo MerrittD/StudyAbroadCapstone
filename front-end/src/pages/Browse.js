b8090e682a1f49a432a8a43d8ebdc5cd2c29de8c
MasonTDaniel <danielm@southwestern.edu>
2020-03-04 00:33:37 -0600

    added comments
    explain how componentDidMount fetches and deals with our data

--- front-end/src/pages/Browse.js
+++ front-end/src/pages/Browse.js
@@ -16,17 +16,21 @@ class Browse extends Component {
         Data passed into an array of object called programs*/
     componentDidMount() {
         let initialPrograms = [];
+        /* Fetch the data from the database */
         fetch('https://my-json-server.typicode.com/MasonTDaniel/capstonedummydata/db')
+            /* Response and promises */
             .then(response => {
                 console.log(response);
                 return response.json();
             })
+            /* Examine the data and then map it to our initialPrograms array */
             .then(data => {
                 console.log(data)
                 initialPrograms = data.programs.map((program) => {
                     return program
                 });
                 console.log(initialPrograms);
+                /* Set our new state with the programs array filled with the programs from the array found in the data */
                 this.setState({
                     programs: initialPrograms,
                 });
