575076bebcdf8a8992772b04e442477f0b6f9360
MasonTDaniel <danielm@southwestern.edu>
2020-03-05 01:18:23 -0600

    added temp admin login
    for client meeting

--- front-end/src/pages/AdminLoginPage.js
+++ front-end/src/pages/AdminLoginPage.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
+import LoginForm from '../components/login/LoginForm'
 import NavBar from '../components/NavBar'
 
-export default function AdminLoginPage() {
-    return (
-        <div>
-            <NavBar />
-        </div>
-    )
+class AdminLoginPage extends Component {
+    render() {
+        return (
+            <div className="main-login-container">
+                <NavBar />
+                <LoginForm />
+            </div>
+        )
+    }
 }
+
+export default AdminLoginPage;
