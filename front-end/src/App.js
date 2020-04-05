c970acfdd47281aab8fe7664066f6c89095f60c8
Mason <masondanielt@gmail.com>
2020-04-05 17:34:15 -0500

    refresh dependencies to try and fix auth errors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, browserHistory } from 'react-router-dom'
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'
 import './App.css';
 import './css/bootstrap.min.css';
 import Home from './pages/Home';
 import HowTo from './pages/HowTo';
 import ContactUs from './pages/ContactUs';
-import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 import ResultPage from './pages/ResultPage';
 import NavBar from './components/NavBar'
+import Login from './pages/Login';
+
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
+
 
 /* Main component which holds the routing to each page */
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <BrowserRouter basename='/'>
+      <Router>
+        <Security issuer='https://dev-228327.okta.com/oauth2/default'
+          clientId='0oa5ecwl6wmPJFY0l4x6'
+          redirectUri={window.location.origin + '/implicit/callback'}
+          onAuthRequired={onAuthRequired}
+          pkce={true} >
           <NavBar />
-          <Switch>
-            <div className="container">
-              <Route exact path="/" component={Home} />
-              <Route path="/ResultPage" component={ResultPage} />
-              <Route path="/how-to" component={HowTo} />
-              <Route path="/contact-us" component={ContactUs} />
-              <Route path="/admin-login" component={AdminLogin} />
-              <Route path="/admin-dashboard" component={AdminDashboard} />
-            </div>
-          </Switch>
-        </BrowserRouter>
-      </div>
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route path="/ResultPage" component={ResultPage} />
+            <Route path="/how-to" component={HowTo} />
+            <Route path="/contact-us" component={ContactUs} />
+            <Route path='/login' render={() => <Login baseUrl='https://dev-228327.okta.com' />} />
+            <Route path='/implicit/callback' component={ImplicitCallback} />
+            <SecureRoute path="/admin-dashboard" component={AdminDashboard} />
+          </div>
+        </Security>
+      </Router>
     );
   }
 }
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -7,13 +7,10 @@ class NavBar extends Component {
         return (
             <div id="nav" className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm">
                 <h5 className="my-0 mr-md-auto font-weight-bold"> <a href="/">Study Abroad</a> </h5>
-
                 <a className="btn" href="/">BROWSE</a>
                 <a className="btn" href="/how-to">HOW TO</a>
                 <a className="btn" href="/contact-us">CONTACT US</a>
-                <a className="btn" href="/admin-login">ADMIN</a>
-
-
+                <a className="btn" href="/admin-dashboard">ADMIN</a>
             </div>
         );
     }
