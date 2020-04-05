05e51ba602353537909dee36463dad0a3faaa0f2
Mason <masondanielt@gmail.com>
2020-04-05 14:39:51 -0500

    fixed navbar buttons

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -8,10 +8,10 @@ class NavBar extends Component {
             <div id="nav" className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm">
                 <h5 className="my-0 mr-md-auto font-weight-bold"> <a href="/">Study Abroad</a> </h5>
 
-                <a className="btn btn-outline-secondary" href="/">BROWSE</a>
-                <a className="btn " href="/how-to">HOW TO</a>
-                <a className="btn " href="/contact-us">CONTACT US</a>
-                <a className="btn " href="/admin-login">ADMIN</a>
+                <a className="btn" href="/">BROWSE</a>
+                <a className="btn" href="/how-to">HOW TO</a>
+                <a className="btn" href="/contact-us">CONTACT US</a>
+                <a className="btn" href="/admin-login">ADMIN</a>
 
 
             </div>
