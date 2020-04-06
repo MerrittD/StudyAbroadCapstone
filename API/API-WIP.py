63e09d99355035e3f744f276c33c1f3a6fd3edd2
Alyssa <casea@southwestern.edu>
2020-04-06 09:36:24 -0500

    added checking verbs to api

--- API/API-WIP.py
+++ API/API-WIP.py
@@ -20,6 +20,18 @@ def home():
     return '''<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
+#checking verbs of incoming request
+@app.route('/check, methods=[GET, POST, PUT, DELETE]')
+def check():
+    if request.method == GET:
+        return "REQUEST TYPE: GET"
+    elif request.method == POST:
+        return "REQUEST TYPE: POST"
+    elif request.method == PUT:
+        return "REQUEST TYPE: PUT"
+    elif request.method == DELETE:
+        return "REQUEST TYPE: DELETE"
+
 
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/books/all', methods=['GET'])
