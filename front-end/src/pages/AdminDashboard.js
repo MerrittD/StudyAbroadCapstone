49145f5aba09a49328a283c445a528f25814ddba
Mason <masondanielt@gmail.com>
2020-04-21 14:45:23 -0500

    get and display working
    had issue with test api url

--- front-end/src/pages/AdminDashboard.js
+++ front-end/src/pages/AdminDashboard.js
@@ -1,42 +1,73 @@
 import React, { Component } from 'react'
+import { Table, Button } from 'reactstrap'
+import axios from 'axios'
 import ProgramList from '../components/ProgramList'
 
-export default class AdminDashboard extends Component {
+class AdminDashboard extends Component {
 
     constructor() {
         super();
         this.state = {
-            programs: [],
+            programs: []
         };
+
     }
 
-    componentDidMount() {
-        let initialPrograms = [];
+    componentWillMount() {
         /* Fetch the data from the database */
-        fetch('https://my-json-server.typicode.com/MasonTDaniel/capstonedummydata/db')
+        axios.get('https://my-json-server.typicode.com/MasonTDaniel/capstonedummydata/allPrograms')
             /* Response and promises */
             .then(response => {
-                console.log(response);
-                return response.json();
-            })
-            /* Examine the data and then map it to our initialPrograms array */
-            .then(data => {
-                console.log(data)
-                initialPrograms = data.allPrograms.map((program) => {
-                    return program
-                });
-                console.log(initialPrograms);
-                /* Set our new state with the programs array filled with the programs from the array found in the data */
+                console.log(response.data);
                 this.setState({
-                    programs: initialPrograms
+                    programs: response.data
                 });
+            }, (error) => {
+                console.log(error);
             });
+        /* Examine the data and then map it to our initialPrograms array */
+
     }
     render() {
+        console.log(this.state)
+        let programs = this.state.programs.map((program) => {
+            return (
+                <tr key={program.id}>
+                    <td>{program.id}</td>
+                    <td>{program.country}</td>
+                    <td>{program.term}</td>
+                    <td>{program.name}</td>
+                    <td>{program.language}</td>
+                    <td>{program.Cost}</td>
+                    <td>
+                        <Button color="success" size="sm" className="mr-2 mb-1">Edit</Button>
+                        <Button color="danger" size="sm">Delete</Button>
+                    </td>
+                </tr>
+            )
+
+        });
         return (
+
             <div>
-                <ProgramList programs={this.state.programs} />
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Country</th>
+                            <th>Term</th>
+                            <th>Name</th>
+                            <th>Language</th>
+                            <th>Cost</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {programs}
+                    </tbody>
+                </Table>
             </div>
         )
     }
 }
+
+export default AdminDashboard;
